c28d114ca009d1b297b75a4d56b454d0228d63fe
Dystian En <distian.enyus@gmail.com>
2024-10-17 15:57:57 +0700

    feat: component popup confirmation

---
+++ src/components/organisms/popup/PopupConfirmation.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'title'
+  }
+})
+const emits = defineEmits(['onClose', 'onConfirm'])
+</script>
+
+<template>
+  <el-dialog :title="props.title" width="500">
+    <slot />
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="emits('onClose')">Cancel</el-button>
+        <el-button type="primary" @click="emits('onConfirm')"> Confirm </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
