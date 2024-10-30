b424eace0fd039266d2aac86499aeceef768e56b
Dystian En <distian.enyus@gmail.com>
2024-10-30 11:55:25 +0700

    test env

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     vue(),
     vueJsx(),
