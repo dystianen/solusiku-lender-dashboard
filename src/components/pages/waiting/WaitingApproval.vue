e7b0200c1143f5d88554aec2bf76dd76de784093
Dystian En <distian.enyus@gmail.com>
2024-11-01 15:11:18 +0700

    fix: padding

--- src/components/pages/waiting/WaitingApproval.vue
+++ src/components/pages/waiting/WaitingApproval.vue
@@ -1,12 +1,9 @@
 <script lang="ts" setup>
 import Waiting from '@/assets/images/waiting.png'
-import useScreenType from '@/composables/useScreenType'
-
-const { isMobile } = useScreenType()
 </script>
 <template>
   <div
-    class="tw-shadow-highlight tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-14 tw-rounded-2xl tw-p-5 tw-text-center"
+    class="tw-py-10 md:tw-py-16 tw-shadow-highlight tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-14 tw-rounded-2xl tw-p-5 tw-text-center"
   >
     <div class="tw-flex tw-flex-col tw-gap-4">
       <h1 class="tw-text-2xl tw-font-bold tw-text-primary lg:tw-text-[42px]">
